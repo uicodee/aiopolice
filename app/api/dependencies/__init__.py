7e2351f501f737ea694cb4d60fdca6c7a0a94a3f
uicodee <uicodee@gmail.com>
2023-04-08 21:47:14 +0500

    authentication and database dependencies

---
+++ app/api/dependencies/__init__.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from sqlalchemy.orm import sessionmaker
+
+from app.api.dependencies.database import DbProvider, dao_provider
+from app.api.dependencies.settings import get_settings
+from app.config import load_config
+
+
+def setup(
+    app: FastAPI,
+    pool: sessionmaker,
+    # settings: Settings,
+):
+    db_provider = DbProvider(pool=pool)
+    # app.dependency_overrides[get_user] = auth_provider.get_current_user
+    app.dependency_overrides[dao_provider] = db_provider.dao
+    # app.dependency_overrides[AuthProvider] = lambda: auth_provider
+    app.dependency_overrides[get_settings] = load_config
